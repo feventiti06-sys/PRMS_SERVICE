c3ff90c57e3af6080eb5bd8f9c3d8ba4ba9e351f
feventiti06-sys <feventiti06@gmail.com>
2026-09-01 23:30:38 -0700

    Fix Swagger OAuth2: Keycloak username/password flow, allow port 8080 CORS

--- src/main/java/com/erp/prms/config/SwaggerConfig.java
+++ src/main/java/com/erp/prms/config/SwaggerConfig.java
@@ -11,29 +11,30 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class SwaggerConfig {
 
-    private static final String OAUTH2_SCHEME = "oauth2";
+    private static final String SCHEME_NAME = "Keycloak";
 
     @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri:http://localhost:8180/realms/prms}")
     private String issuerUri;
 
     @Bean
+    @Primary
     OpenAPI prmsOpenApi() {
         String tokenUrl = issuerUri + "/protocol/openid-connect/token";
 
-        Scopes scopes = new Scopes()
-                .addString("openid", "OpenID Connect")
-                .addString("profile", "User profile")
-                .addString("email", "User email");
-
         OAuthFlow passwordFlow = new OAuthFlow()
                 .tokenUrl(tokenUrl)
-                .scopes(scopes);
+                .scopes(new Scopes()
+                        .addString("openid", "OpenID")
+                        .addString("profile", "Profile")
+                        .addString("email", "Email"));
 
-        SecurityScheme oauth2Scheme = new SecurityScheme()
+        SecurityScheme keycloakScheme = new SecurityScheme()
+                .name(SCHEME_NAME)
                 .type(SecurityScheme.Type.OAUTH2)
                 .flows(new OAuthFlows().password(passwordFlow));
 
@@ -41,8 +42,9 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("PRMS API")
                         .version("1.0")
-                        .description("Procurement Resource Management System — INSA"))
-                .addSecurityItem(new SecurityRequirement().addList(OAUTH2_SCHEME))
-                .components(new Components().addSecuritySchemes(OAUTH2_SCHEME, oauth2Scheme));
+                        .description("Procurement Resource Management System — INSA ERP"))
+                .addSecurityItem(new SecurityRequirement().addList(SCHEME_NAME))
+                .components(new Components()
+                        .addSecuritySchemes(SCHEME_NAME, keycloakScheme));
     }
 }
