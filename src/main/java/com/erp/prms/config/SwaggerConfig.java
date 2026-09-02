be4b310c8a98f8090a5934d3f4e48b1bf78c20eb
feventiti06-sys <feventiti06@gmail.com>
2026-09-01 23:21:00 -0700

    Swagger OAuth2 password flow with Keycloak — username/password in Authorize dialog

--- src/main/java/com/erp/prms/config/SwaggerConfig.java
+++ src/main/java/com/erp/prms/config/SwaggerConfig.java
@@ -3,28 +3,46 @@ package com.erp.prms.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.OAuthFlow;
+import io.swagger.v3.oas.models.security.OAuthFlows;
+import io.swagger.v3.oas.models.security.Scopes;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
-    private static final String BEARER_AUTH_SCHEME = "bearerAuth";
+    private static final String OAUTH2_SCHEME = "oauth2";
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri:http://localhost:8180/realms/prms}")
+    private String issuerUri;
 
     @Bean
     OpenAPI prmsOpenApi() {
+        String tokenUrl = issuerUri + "/protocol/openid-connect/token";
+
+        Scopes scopes = new Scopes()
+                .addString("openid", "OpenID Connect")
+                .addString("profile", "User profile")
+                .addString("email", "User email");
+
+        OAuthFlow passwordFlow = new OAuthFlow()
+                .tokenUrl(tokenUrl)
+                .scopes(scopes);
+
+        SecurityScheme oauth2Scheme = new SecurityScheme()
+                .type(SecurityScheme.Type.OAUTH2)
+                .flows(new OAuthFlows().password(passwordFlow));
+
         return new OpenAPI()
                 .info(new Info()
                         .title("PRMS API")
-                        .version("v1")
-                        .description("Purchase Requisition Management System API"))
-                .addSecurityItem(new SecurityRequirement().addList(BEARER_AUTH_SCHEME))
-                .components(new Components().addSecuritySchemes(BEARER_AUTH_SCHEME,
-                        new SecurityScheme()
-                                .type(SecurityScheme.Type.HTTP)
-                                .scheme("bearer")
-                                .bearerFormat("JWT")));
+                        .version("1.0")
+                        .description("Procurement Resource Management System — INSA"))
+                .addSecurityItem(new SecurityRequirement().addList(OAUTH2_SCHEME))
+                .components(new Components().addSecuritySchemes(OAUTH2_SCHEME, oauth2Scheme));
     }
 }
